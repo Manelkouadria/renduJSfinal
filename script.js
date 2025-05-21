ba8635f154ba1f62857af348174b04eed12b5594
Manelkouadria <MANEL.KOUADRIA@latoile.em-lyon.com>
2025-05-21 14:05:35 +0200

    commit1

--- script.js
+++ script.js
@@ -44,13 +44,29 @@ fetch('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.g
     data.avantagesClients.forEach(element => {
     
         const avantagesClients = document.createElement("h3");
-         avantagesClients.classList.add("avantagecli");
+         avantagesClients.classList.add("avantageclients");
 
     avantagesClients.textContent = data.avantagesClients;
     dispatchEvent.classList = "avantagesClients"
     avantage.appendChild(avantagesClients);
-   
 
+    //niveau 1e2 fini.
+
+//  Pour chaque activité dans les données JSON, crée dynamiquement une div contenant un titre, une description et une image.
+
+// Affiche ces informations dans une section dédiée, avec une carte pour chaque activité si les données contiennent un lien vers une image.
+   
+const section1= document.createElement("section")
+const div1 = document.createElement("div")
+const h21 =document.createElement ("h2")
+const p2= document.createElement("p")
+const img1 = document.createElement("img")
+
+principale.appendChild(section1);
+section1.appendChild(div1)
+section1.appendChild
+section1.appendChild
+section1.appendChild
 
 });
     })
